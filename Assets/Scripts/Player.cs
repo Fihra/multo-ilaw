d5b1fc784bf897b4add0e2e241299c7d588262cc
Fihra <fabrofabian@gmail.com>
2021-05-23 23:10:49 -1000

    character can move up and down

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector2 targetPos;
+    public float Yincrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.W) && transform.position.y < maxHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+        }
+        else if(Input.GetKeyDown(KeyCode.S) && transform.position.y > minHeight)
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+        }
     }
 }

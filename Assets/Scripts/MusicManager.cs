e400b1866ff917a608d8dea4a383287221bd34f0
Fihra <fabrofabian@gmail.com>
2021-06-16 10:55:59 -1000

    title and forest theme implemented

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    Scene currentScene;
+
+    public AK.Wwise.Event musicEvent;
+    public List<AK.Wwise.State> musicStates = new List<AK.Wwise.State>();
+
+    private void Awake()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        musicStates[0].SetValue();
+        musicEvent.Post(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name == "GameScene")
+        {
+            musicStates[1].SetValue();
+
+        }
+        Debug.Log(currentScene.name);
+    }
+}

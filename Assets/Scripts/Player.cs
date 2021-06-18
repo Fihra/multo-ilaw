80265584e22910a49305b260ed7e14fecb493e25
Fihra <fabrofabian@gmail.com>
2021-06-17 16:08:11 -1000

    added phase along with ui slider that is controlled up and down on spacebar press

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,6 +16,9 @@ public class Player : MonoBehaviour
     public int health;
     //public Text healthDisplay;
     public int numOfLights;
+    float phase = 1.0f;
+
+    public Slider phaseSlider;
 
     public Image[] lights;
     public Sprite fullLight;
@@ -33,20 +36,20 @@ public class Player : MonoBehaviour
     {
         colorHolder = GetComponent<SpriteRenderer>();
         playerTrigger = GetComponent<BoxCollider2D>();
+        phaseSlider.value = phase;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ManageHealth()
     {
-        if(health > numOfLights)
+        if (health > numOfLights)
         {
             health = numOfLights;
         }
 
         //healthDisplay.text = "HP: " + health.ToString();
-        for(int i = 0; i < lights.Length; i++)
+        for (int i = 0; i < lights.Length; i++)
         {
-            if(i < health)
+            if (i < health)
             {
                 lights[i].sprite = fullLight;
             }
@@ -56,7 +59,7 @@ public class Player : MonoBehaviour
             }
 
 
-            if(i < numOfLights)
+            if (i < numOfLights)
             {
                 lights[i].enabled = true;
             }
@@ -66,13 +69,16 @@ public class Player : MonoBehaviour
             }
         }
 
-        if(health <= 0)
+        if (health <= 0)
         {
             Instantiate(effect, transform.position, Quaternion.identity);
             gameOver.SetActive(true);
             Destroy(gameObject);
         }
+    }
 
+    void Movement()
+    {
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && transform.position.y < maxHeight)
@@ -80,19 +86,29 @@ public class Player : MonoBehaviour
             Instantiate(moveUpEffect, transform.position, Quaternion.identity);
             targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
         }
-        else if((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && transform.position.y > minHeight)
+        else if ((Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)) && transform.position.y > minHeight)
         {
             Instantiate(moveDownEffect, transform.position, Quaternion.identity);
             targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
         }
-        else if(Input.GetKeyDown(KeyCode.Space))
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ManageHealth();
+        Movement();
+
+        if(Input.GetKey(KeyCode.Space) && phaseSlider.value > 0)
         {
             colorHolder.color = new Color(1f, 1f, 1f, 0.5f);
+            phaseSlider.value -= 1f * Time.deltaTime;
             playerTrigger.isTrigger = false;
         }
-        else if(Input.GetKeyUp(KeyCode.Space))
+        else
         {
             colorHolder.color = new Color(1f, 1f, 1f, 1f);
+            phaseSlider.value += 1f * Time.deltaTime;
             playerTrigger.isTrigger = true;
         }
     }

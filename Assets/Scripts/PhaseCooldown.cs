9eb32c227bf66b295e7823ec9e52e8a16cfe540f
Fihra <fabrofabian@gmail.com>
2021-06-19 09:45:02 -1000

    added RTPC to control cooldown sfx recover

---
+++ Assets/Scripts/PhaseCooldown.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhaseCooldown : MonoBehaviour
+{
+    public AK.Wwise.Event cooldownEvent;
+
+    public uint cooldownSoundID;
+
+    private Slider phaseSlider;
+    private float phaseSliderValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        phaseSlider = gameObject.GetComponent<Slider>();
+        phaseSliderValue = phaseSlider.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        phaseSliderValue = phaseSlider.value;
+        //Debug.Log(phaseSliderValue);
+
+        if(phaseSliderValue == 1f)
+        {
+            AkSoundEngine.SetRTPCValue("Cooldown", 0f);
+        }
+        
+        if (phaseSliderValue == 0f)
+        {
+            cooldownSoundID = AkSoundEngine.PostEvent("Cooldown", gameObject);
+            AkSoundEngine.SetRTPCValue("Cooldown", 100.0f);
+           
+        }
+    }
+}
